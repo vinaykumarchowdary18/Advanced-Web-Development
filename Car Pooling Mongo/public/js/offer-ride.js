121bca92502d77b789f92559b8bb2ffdf0a0b206
Vinay Kumar <119227345+vinaykumarchowdary18@users.noreply.github.com>
2025-05-02 16:03:51 +0530

    Delete Car Pooling Mongo directory

--- Car Pooling Mongo/public/js/offer-ride.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('offerRideForm');
-    
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        alert('Ride offer submitted!');
-        // In a real app, you would send this data to your server
-    });
-});
--- Car Pooling Mongo/readme.txt
+++
@@ -1,7 +0,0 @@
-npm install node   //for node js
-
-npm install express // to install express 
-
-download mongobd from and mongoshell from https://www.mongodb.com/try/download/community
-run the commands mongod
-mongosh
